73df6db7207793bf2c5c71e85a516c38adc7e6ca
baconator <bacon@noreply.users.github.com>
2015-03-26 18:36:26 -0700

    Added a second sphere.

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,17 @@ int main() {
 
     Eigen::Vector4f pink(1.0f, 0.3f, 1.0f, 1.0f);
     MaterialParameters matParams{pink, pink, pink, 0.0f};
+    Eigen::Vector4f blue(0.3f, 0.3f, 1.0f, 1.0f);
+    MaterialParameters matParams2{blue, blue, blue, 5.0f};
 
     std::vector<Intersectable*> geometry;
+    Sphere sph2(Eigen::Vector4f(-2.0f, 0.0f, 5.0f, 0.0f), 3.0f, matParams2);
     Sphere sph(Eigen::Vector4f(0.0f, 1.0f, 12.0f, 0.0f), 10.0f, matParams);
     geometry.push_back(&sph);
+    geometry.push_back(&sph2);
 
     std::vector<Light> lights;
-    Light lht(Eigen::Vector4f(20.0f, 3.0f, 2.0f, 0.0f), Eigen::Vector4f(1.0f, 1.0f, 1.0f, 1.0f));
+    Light lht(Eigen::Vector4f(10.0f, 1.0f, 3.0f, 0.0f), Eigen::Vector4f(1.0f, 1.0f, 1.0f, 1.0f));
     lights.push_back(lht);
 
     Scene scene(geometry, lights, camera, parameters);
